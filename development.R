f09144fdc1cb6947f10d7845bc9f67fc83a9e561
David Blaa <dblaa@bgc-jena.mpg.de>
2018-11-13 10:46:37 +0100

    Update Scripts to BEXIS Version 2.12.1 - add all required packages

--- development.R
+++ development.R
@@ -1,8 +1,26 @@
 # This is a development helper file. It hosts calls to help with the
 # documentation and package checking and to try out new functions.
 
+# install devtools
+install.packages("devtools")
+# install rBExIS package from github
+library(devtools)
+install_github("BEXIS2/rBExIS", subdir = "rBExIS")
+
 # load the devtools package
 require("devtools")
+
+#load httr 
+install.packages("httr")
+
+#Require the package so you can use it
+require("httr")
+
+#load jsonlite
+install.packages("jsonlite")
+
+#Require the package so you can use it
+require("jsonlite")
 
 # load/reload the bexis package functions
 load_all("rBExIS")
@@ -15,7 +33,9 @@ require(rBExIS)
 
 ## options command to query and set rbexis options
 bexis.options()
-bexis.options("base_url" = "http://bexis.inf-bb.uni-jena.de:2201")
+bexis.options("token" = "VUFDGJKFYETpzExHKesCA59JRX7fALE99VFF9vJogbzzSsaaU8HRJHw7r5pn6Bdb")
+bexis.options("base_url" = "https://localhost:44345")
 
 # get data from bexis
-bexis.get.dataset_by(id = 1)
+bexis.get.dataset_by(id = 9)
+
